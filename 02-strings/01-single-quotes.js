550784b4a4d0f86e0ecb7d7724b5f992cefff2a7
Javier Alvarado <jdrummer1@outlook.com>
2026-03-12 12:49:20 -0700

    Update strings section

---
+++ 02-strings/01-single-quotes.js
@@ -0,0 +1,9 @@
+// Single Quotes
+
+// Run: node 02-strings/01-string-variables.js
+
+// Learn how to create and store text in string variables
+
+let message = 'Hello World';
+
+console.log(message);
